60dd2d32b918508ffbfbad8fbb8815df281395ab
Artsiom Kashkou <schetka40@gmail.com>
2024-12-19 16:17:08 +0100

    Prepare project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // default version
+  trailingSlash: true,
+  async redirects() {
+    const allRedirects = [
+      { source: "/test/", destination: "/", permanent: true },
+    ];
+    return allRedirects;
+  },
+};
+
+// V2
+// const nextConfig = {
+//   trailingSlash: true,
+//   async redirects() {
+//     return [
+//       { source: "/test/", destination: "/", permanent: true },
+//       { source: "/", destination: "/", permanent: true },
+//     ];
+//   },
+// };
+
+// V3
+// const nextConfig = {
+//   trailingSlash: true,
+//   async redirects() {
+//     return [
+//       { source: "/test/", destination: "/", permanent: true },
+//       {
+//         source: "/:path((?!.+\\.[\\w]+$)(?!.*/$).*)",
+//         destination: "/:path/",
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
 
 export default nextConfig;
